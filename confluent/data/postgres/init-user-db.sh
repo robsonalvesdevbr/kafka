e57e32f692c020b2fd8fc8a7e504d62e3d52546b
Robson Alves <contato@robsonalves.dev.br>
2025-05-08 10:34:43 -0300

    feat: add PostgreSQL initialization script and volume configuration to Docker Compose

---
+++ confluent/data/postgres/init-user-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER kafka WITH PASSWORD 'kafka@2025';
+	CREATE DATABASE kafkadb;
+	GRANT ALL PRIVILEGES ON DATABASE kafkadb TO kafka;
+    \c kafkadb;
+    CREATE TABLE client (
+        id int PRIMARY KEY,
+        name VARCHAR(100) NOT NULL
+    );
+EOSQL
