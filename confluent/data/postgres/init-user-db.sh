08975325d17f2e00aa7c872425c6e05d5ef60257
Robson Alves <contato@robsonalves.dev.br>
2025-05-08 14:44:41 -0300

    feat: update user database initialization script to use SERIAL for id and grant privileges on all tables and sequences

--- confluent/data/postgres/init-user-db.sh
+++ confluent/data/postgres/init-user-db.sh
@@ -7,8 +7,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	GRANT ALL PRIVILEGES ON DATABASE kafkadb TO kafka;
     \c kafkadb;
     CREATE TABLE client (
-        id int PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL
     );
-    GRANT ALL PRIVILEGES ON TABLE client TO kafka;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO kafka;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO kafka;
 EOSQL
