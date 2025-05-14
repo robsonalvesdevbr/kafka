d9e5b88b7f4e836b02b2bc3c6fe5dd2791d69a78
Robson Alves <contato@robsonalves.dev.br>
2025-05-14 09:31:42 -0300

    feat: adicionar configuração do conector JDBC para PostgreSQL e criar tabela no banco de dados

--- confluent/data/postgres/init-user-db.sh
+++ confluent/data/postgres/init-user-db.sh
@@ -8,8 +8,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     \c kafkadb;
     CREATE TABLE client (
         id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL
+        name VARCHAR(100) NOT NULL,
+        hour_test varchar(5) default '00:00' not null
     );
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO kafka;
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO kafka;
+    INSERT INTO client (name, hour_test) values ('Robson Candido dos Santos Alves', TO_CHAR(NOW(), 'HH24:MI'));
 EOSQL
